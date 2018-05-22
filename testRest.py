961752cac7ade1b2182e68a3037892509c6b10a0
RWare12 <39044631+RWare12@users.noreply.github.com>
2018-05-22 15:14:56 +0800

    Update testRest.py

--- testRest.py
+++ testRest.py
@@ -1,10 +1,4 @@
-#Original Source code
-
-import sys, os, re, shutil, json, urllib, urllib2, BaseHTTPServer
-
-# Fix issues with decoding HTTP responses
-reload(sys)
-sys.setdefaultencoding('utf8')
+import sys, os, re, shutil, json, urllib.parse, urllib.request, http.server
 
 here = os.path.dirname(os.path.realpath(__file__))
 
@@ -12,19 +6,19 @@ records = {}
 
 def get_records(handler):
     return records
-
+#added .parse instead of urllib.unqoute
 def get_record(handler):
-    key = urllib.unquote(handler.path[8:])
+    key = urllib.parse.unquote(handler.path[8:])
     return records[key] if key in records else None
 
 def set_record(handler):
-    key = urllib.unquote(handler.path[8:])
+    key = urllib.parse.unquote(handler.path[8:])
     payload = handler.get_payload()
     records[key] = payload
     return records[key]
 
 def delete_record(handler):
-    key = urllib.unquote(handler.path[8:])
+    key = urllib.parse.unquote(handler.path[8:])
     del records[key]
     return True # anything except None shows success
 
@@ -34,14 +28,14 @@ def rest_call_json(url, payload=None, with_payload_method='PUT'):
         if not isinstance(payload, basestring):
             payload = json.dumps(payload)
         # PUT or POST
-        response = urllib2.urlopen(MethodRequest(url, payload, {'Content-Type': 'application/json'}, method=with_payload_method))
+        response = urllib3.urlopen(MethodRequest(url, payload, {'Content-Type': 'application/json'}, method=with_payload_method))
     else:
         # GET
-        response = urllib2.urlopen(url)
+        response = urllib3.urlopen(url,payload)
     response = response.read().decode()
     return json.loads(response)
 
-class MethodRequest(urllib2.Request):
+class MethodRequest(urllib.request.Request):
     'See: https://gist.github.com/logic/2715756'
     def __init__(self, *args, **kwargs):
         if 'method' in kwargs:
@@ -49,19 +43,19 @@ class MethodRequest(urllib2.Request):
             del kwargs['method']
         else:
             self._method = None
-        return urllib2.Request.__init__(self, *args, **kwargs)
+        return urllib3.Request.__init__(self, *args, **kwargs)
 
     def get_method(self, *args, **kwargs):
-        return self._method if self._method is not None else urllib2.Request.get_method(self, *args, **kwargs)
+        return self._method if self._method is not None else urllib3.Request.get_method(self, *args, **kwargs)
 
-class RESTRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+class RESTRequestHandler(http.server.BaseHTTPRequestHandler):
     def __init__(self, *args, **kwargs):
         self.routes = {
             r'^/$': {'file': 'web/index.html', 'media_type': 'text/html'},
             r'^/records$': {'GET': get_records, 'media_type': 'application/json'},
             r'^/record/': {'GET': get_record, 'PUT': set_record, 'DELETE': delete_record, 'media_type': 'application/json'}}
         
-        return BaseHTTPServer.BaseHTTPRequestHandler.__init__(self, *args, **kwargs)
+        return http.server.BaseHTTPRequestHandler.__init__(self, *args, **kwargs)
     
     def do_HEAD(self):
         self.handle_method('HEAD')
@@ -77,11 +71,11 @@ class RESTRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
 
     def do_DELETE(self):
         self.handle_method('DELETE')
-    
+    #changed getheader to get_all
     def get_payload(self):
-        payload_len = int(self.headers.getheader('content-length', 0))
-        payload = self.rfile.read(payload_len)
-        payload = json.loads(payload)
+        payload_len = self.headers.get_all('content-length',0)
+        payload = self.rfile.read(int(payload_len[0]))
+        payload = json.loads(payload.decode('utf-8').replace("'","\""))
         return payload
         
     def handle_method(self, method):
@@ -126,7 +120,7 @@ class RESTRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
                                 self.send_header('Content-type', route['media_type'])
                             self.end_headers()
                             if method != 'DELETE':
-                                self.wfile.write(json.dumps(content))
+                                self.wfile.write(json.dumps(content).encode("utf-8"))
                         else:
                             self.send_response(404)
                             self.end_headers()
@@ -136,22 +130,22 @@ class RESTRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
                         self.end_headers()
                         self.wfile.write(method + ' is not supported\n')
                     
-    
+    #changed iteritems to items
     def get_route(self):
-        for path, route in self.routes.iteritems():
+        for path, route in self.routes.items():
             if re.match(path, self.path):
                 return route
         return None
 
 def rest_server(port):
     'Starts the REST server'
-    http_server = BaseHTTPServer.HTTPServer(('', port), RESTRequestHandler)
-    print 'Starting HTTP server at port %d' % port
+    http_server = http.server.HTTPServer(('', port), RESTRequestHandler)
+    print ('Starting HTTP server at port %d' % port)
     try:
         http_server.serve_forever()
     except KeyboardInterrupt:
         pass
-    print 'Stopping HTTP server'
+    print ('Stopping HTTP server')
     http_server.server_close()
 
 def main(argv):
